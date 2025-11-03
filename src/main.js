6205f0f6af6003ac24337f513f709d68ec92570e
Abrham Aragie <145662971+admarspia@users.noreply.github.com>
2025-11-04 01:15:27 +0300

    Refactor game loop and input handling

--- src/main.js
+++ src/main.js
@@ -4,34 +4,6 @@ import Input from "../src/js/engine/Input.js";
 import Maze from "../src/js/maze/maze.js";
 import ThirdPersonCamera from "../src/js/engine/Camera.js";
 
-<<<<<<< HEAD
-const renderer = new Renderer();
-const maze = new Maze(15, 15);
-maze.generate(); renderer.createMaze(maze.getGrid(), 4, 2);
-const tank = new TankBody(0, 0.5, 0); renderer.scene.add(tank.mesh);
-const input = new Input(); const bullets = [];
-let lastTime = 0; const offset = new THREE.Vector3(0, 2, -5);
-const tankDirection = new THREE.Vector3(); const tankPosition = new THREE.Vector3();
-
-function gameLoop(time) { const delta = (time - lastTime) / 1000; lastTime = time;
-                         if (input.isDown("ArrowUp")) tank.moveForward(delta);
-                         if (input.isDown("ArrowDown")) tank.moveBackward(delta);
-                         if (input.isDown("ArrowLeft")) tank.rotateLeft(delta);
-                         if (input.isDown("ArrowRight")) tank.rotateRight(delta);
-                         if (input.isDown("i")) tank.turret.barrel.aimUp(delta);
-                         if (input.isDown("k")) tank.turret.barrel.aimDown(delta);
-                         if (input.isDown("j")) tank.turret.rotateLeft(delta);
-                         if (input.isDown("l")) tank.turret.rotateRight(delta);
-                         if (input.isDown("x")) tank.turret.barrel.shoot(bullets, renderer.scene);
-                         
-                         for (let i = bullets.length - 1; i >= 0; i--) { 
-                                 const bullet = bullets[i]; bullet.update(delta);
-                                  if (!bullet.active) bullets.splice(i, 1); 
-                         }
-                         renderer.render();
-                         requestAnimationFrame(gameLoop);
-  } 
-=======
 const renderer = new Renderer();  
 const input = new Input();
 const maze = new Maze(15, 15);
@@ -78,4 +50,3 @@ function gameLoop(time) {
 }
 
 requestAnimationFrame(gameLoop);
-
