70ca3a4ac9a9c0a76c9ae94e63520b14507885e8
illusionsp <aschalewgetahun238@gmail.com>
2025-11-25 13:25:39 +0300

    Fixed entry page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import Game from "../src/js/engine/Game.js";
+import '/src/css/style.css';
+import Game from './js/engine/Game.js'; // Remove the curly braces { }
+
+const startScreen = document.getElementById('start-screen');
+const playBtn = document.getElementById('play-btn');
 
 const game = new Game();
-game.start();
 
+playBtn.addEventListener('click', () => {
+    startScreen.style.display = 'none';
+    if (typeof game.start === "function") game.start();
+    console.log("Game Started!");
+});
