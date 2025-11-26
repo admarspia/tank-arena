808f81da03478113d0676523b7a328eab37435f5
illusionsp <aschalewgetahun238@gmail.com>
2025-11-26 10:51:39 +0300

    the game area shown after click the palynow button

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import '/src/css/style.css';
-import Game from './js/engine/Game.js'; 
+import Game from './js/engine/Game.js';
 
 const startScreen = document.getElementById('start-screen');
 const playBtn = document.getElementById('play-btn');
 
-const game = new Game();
-
 playBtn.addEventListener('click', () => {
     startScreen.style.display = 'none';
-    if (typeof game.start === "function") game.start();
+
+    const game = new Game();
+
+    if (typeof game.start === "function") {
+        game.start();
+    }
+
     console.log("Game Started!");
 });
