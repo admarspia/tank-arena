dbc088f9922ba2056b5c3c974ac6fe0bfb689270
illusionsp <aschalewgetahun238@gmail.com>
2025-11-27 20:29:59 +0300

    add cntrol page background image

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import './css/style.css';
 import Game from './js/engine/Game.js';
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -8,22 +9,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const playBtn = document.getElementById("play-btn");
     const controlsPlayBtn = document.getElementById("controls-play-btn");
 
-    // Landing page -> Controls page
     playBtn.addEventListener("click", () => {
         startScreen.style.display = "none";
-        controlsPage.style.display = "flex";
+        controlsPage.style.display = "flex"; 
     });
 
-    // Controls page -> Start game
     controlsPlayBtn.addEventListener("click", () => {
         controlsPage.style.display = "none";
-
-        // Show game area first
         gameArea.style.display = "block";
+
         gameArea.style.width = window.innerWidth + "px";
         gameArea.style.height = window.innerHeight + "px";
 
-        // Start the game
         const game = new Game();
         game.start();
 
